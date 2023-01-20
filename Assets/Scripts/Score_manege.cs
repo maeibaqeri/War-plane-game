f339e3937b32fcb644a3e5640ee15b03952c4be5
made <maede.baghei1234@gmail.com>
2023-01-20 15:36:07 +0330

    fateme jafari
    Changes:
    Points are added every two seconds

--- Assets/Scripts/Score_manege.cs
+++ Assets/Scripts/Score_manege.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score_manege : MonoBehaviour
 {
+    public int score=0;
+    public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Score());
     }
 
     // Update is called once per frame
     void Update()
     {
+       scoreText.text=score.ToString();
+    }
+    IEnumerator Score(){
+    while (true)
+    {
+            yield return new WaitForSeconds(2);
+         score=score+1;
+        Debug.Log("Score: "+ score);
+        
+    }
         
     }
 }
